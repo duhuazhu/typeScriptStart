bf426e7c1efedb84bff613b8b194cf30f21b129d
“duhuazhu” <335921101@qq.com>
2022-07-04 21:20:56 +0800

    ts 基本配置项

--- code/chapter01/part2/app.ts
+++ code/chapter01/part2/app.ts
@@ -2,4 +2,20 @@ console.log('hello world');
 let a = 10;
 let b = 10;
 console.log(b);
+
+function fn(a:number,b:number){
+    return a + b;
+}
+
+function fn2(this:Window){
+    alert(this)
+}
+
+let box1  =document.getElementById('box1');
+//strictNullChecks 设置为true  得判断是否为空
+box1?.addEventListener('click', function(e){
+
+})
+
+
 export const hello = 'hello world';
